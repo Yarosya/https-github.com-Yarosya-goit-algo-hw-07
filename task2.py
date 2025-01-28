cc39606f6d0a55bf7b55a4d06d644a9b48e278e9
Yarosya <rozvazkyi1999@gmail.com>
2025-01-29 00:43:05 +0200

    task 2

---
+++ task2.py
@@ -0,0 +1,93 @@
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def get_height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def get_balance(self, node):
+        if not node:
+            return 0
+        return self.get_height(node.left) - self.get_height(node.right)
+
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+
+        return y
+
+    def right_rotate(self, y):
+        x = y.left
+        T3 = x.right
+
+        x.right = y
+        y.left = T3
+
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+
+        return x
+
+    def insert(self, root, key):
+        if not root:
+            return AVLNode(key)
+
+        if key < root.key:
+            root.left = self.insert(root.left, key)
+        elif key > root.key:
+            root.right = self.insert(root.right, key)
+        else:
+            return root
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+
+        balance = self.get_balance(root)
+
+        if balance > 1:
+            if key < root.left.key:
+                return self.right_rotate(root)
+            else:
+                root.left = self.left_rotate(root.left)
+                return self.right_rotate(root)
+
+        if balance < -1:
+            if key > root.right.key:
+                return self.left_rotate(root)
+            else:
+                root.right = self.right_rotate(root.right)
+                return self.left_rotate(root)
+
+        return root
+
+    def find_min(self, root):
+        if not root:
+            return None
+
+        while root.left:
+            root = root.left
+
+        return root.key
+
+avl_tree = AVLTree()
+
+keys = [20, 10, 30, 25, 40, 50]
+for key in keys:
+    avl_tree.root = avl_tree.insert(avl_tree.root, key)
+
+min_value = avl_tree.find_min(avl_tree.root)
+print("Найменше значення в AVL-дереві:", min_value)
